aca391b319368d2e4d0f3f30f909ea1fa6862255
Vinay Patil <vpatil@adaequare.com>
2017-02-28 18:48:02 +0530

    Assignment work

---
+++ Assigment.Web/Controllers/FolksController.cs
@@ -0,0 +1,34 @@
+﻿using Assigment.Interface;
+using Assignment.Models;
+using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assigment.Web.Controllers
+{
+    public class FolksController : Controller
+    {
+        // GET: Folks
+        public static IFolksService _iFolksService;
+        public FolksController()
+        {
+            // _iFolksService = folksService;
+            _iFolksService = ObjectFactory.GetInstance<IFolksService>();
+        }
+        public ActionResult Index()
+        {
+            List<FolksViewModel> resultObj = _iFolksService.GetAllFolksDetails();
+            var _folksDetails = new FolksViewModel();
+            _folksDetails.FolksDetails = resultObj;
+            return View(_folksDetails);
+        }
+        public JsonResult GetAllFolksDetails()
+        {
+            List<FolksViewModel> resultObj = _iFolksService.GetAllFolksDetails();
+            return Json(new { data = resultObj }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Assigment.Web/DependencyResolution/IoC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Assigment.Interface;
+using Assigment.Repository;
+using StructureMap;
+using StructureMap.Graph;
+using Assigment.Data;
+
+namespace Assigment.Web.DependencyResolution
+{
+    public static class IoC
+    {
+        public static IContainer Initialize()
+        {
+            ObjectFactory.Initialize(x =>
+            {
+                x.Scan(scan =>
+                {
+                    scan.TheCallingAssembly();
+                    scan.WithDefaultConventions();
+                });
+                x.For<IFolksService>().Use<FolksRepository>();
+                x.For<IQueryServices>().Use<QueryRepository>();
+            });
+            return ObjectFactory.Container;
+        }
+    }
+}
